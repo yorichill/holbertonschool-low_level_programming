c1d23906aff76520a599efa0f8a1f4d0c35f6c5e
yorichill <thomas.haenel101@gmail.com>
2025-10-22 11:06:28 +0000

    zz

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        if (ch != 'e' && ch != 'q')
+            putchar(ch);
+    }
+    putchar('\n');
+    return (0);
+}
 
