a42848edc3686db1a74bee25f012292419157916
yorichill <thomas.haenel101@gmail.com>
2025-10-23 12:35:38 +0200

    e

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,18 @@
-#define main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c); 
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);  /* <--- ici */
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-
+#endif
