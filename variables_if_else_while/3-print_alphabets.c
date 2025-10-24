4c2fce0d6b56609a0a68a18f6a33cc7e5aaee18b
yorichill <thomas.haenel101@gmail.com>
2025-10-24 11:35:19 +0200

    e

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, then uppercase, 
+ * main - prints the alphabet in lowercase, then uppercase,
  *
  * Return: Always 0 (Success)
  */
