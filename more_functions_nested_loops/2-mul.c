686d6b9df2aef8a64fcdd1587ba05529d851a6f7
yorichill <thomas.haenel101@gmail.com>
2025-10-27 11:31:25 +0100

    e

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -  that multiplies two integers.
+ * @c: the character to check
+ *
+ * Return: Always the multiplies of two integers.
+ */
+
+int mul(int a, int b)
+{
+
+}
