6d2384dc81c050624e820afc6f1fc06adeb125d2
yorichill <thomas.haenel101@gmail.com>
2025-10-27 12:53:57 +0100

    3939

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,17 @@
  *
  * Return: nothing
  */
-void print_most_numberss(void)
+void print_most_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(i + '0');
-      
-	}
-    if (i == 2 || i == 4)
-    continue;
-	_putchar('\n');
+    for (i = 0; i <= 9; i++)
+    {
+        if (i == 2 || i == 4)
+            continue; 
+
+        _putchar(i + '0');
+    }
+
+    _putchar('\n');
 }
