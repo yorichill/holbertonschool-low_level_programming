85589cfb3541cfb97f10f536a94a6f5a45a31d89
yorichill <thomas.haenel101@gmail.com>
2025-10-27 13:01:47 +0100

    3939

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+for (int i = 0; i < 10; i++)
+{
+    
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
