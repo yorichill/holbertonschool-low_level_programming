6d5b22025c9c50947cb0498940f138350304c7b4
yorichill <thomas.haenel101@gmail.com>
2025-10-27 13:15:34 +0100

    3939

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * print_line - it draws a straight line in the terminal
+ *
+ * Return: nothing
+ */
+
+ void print_line(int n)
+{
+int n;
+
+for (; n != 0; n--)
+{
+if (n = 0)
+_putchar('\n');
+
+_putchar(_);
+}
+}
