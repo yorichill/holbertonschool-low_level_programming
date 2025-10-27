7625ca189b19e14c705c67daf5ea29e421b5a5a8
yorichill <thomas.haenel101@gmail.com>
2025-10-27 12:33:14 +0100

    print 0 to 9 with 2 putchar max

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers -  print using 2 putchar
+ * @i: runs through the chain
+ * Return: all number
+ */
+{
+char text[] = "0123456789";
+int i;
+
+for (i = 0; text[i] != '\0'; i++)
+		_putchar(text[i]);
+	_putchar('\n');
+
+	return (0);
+}
