e803b25bb98548e3aba2169d916a8b429d719a11
yorichill <thomas.haenel101@gmail.com>
2025-10-27 12:48:58 +0100

    add proto

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9 except 2 and 4
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+      
+	}
+    if (i = 2 || i = 4 )
+    continue;
+	_putchar('\n');
+}
