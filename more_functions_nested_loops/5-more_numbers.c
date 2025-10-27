a0cd5cb55683b9c3b333b58ceb26ca59aa373f94
yorichill <thomas.haenel101@gmail.com>
2025-10-27 13:04:40 +0100

    3939

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,23 @@
  * Return: nothing
  */
 void more_numbers(void)
-for (int i = 0; i < 10; i++)
 {
-    
-	int i;
+int i, j;
 
-	for (i = 0; i <= 14; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
-
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+_putchar('1');
+_putchar(j % 10 + '0');
+}
+else
+{
+_putchar(j + '0');
+}
+}
+_putchar('\n');
+}
 }
