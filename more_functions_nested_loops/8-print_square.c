6c64556ab944c86643b933c78589f45fd5fdd5b1
yorichill <thomas.haenel101@gmail.com>
2025-10-28 10:34:17 +0100

    3939

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,20 @@
  */
 void print_square(int size)
 {
-    int i, j;
+int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
