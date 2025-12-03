5db3190f9cc27cb813a08b75bf27099b2a5515ca
yorichill <thomas.haenel101@gmail.com>
2025-12-03 10:54:17 +0100

    add lists.h

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h> /* Pour le type size_t */
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/*
+ * Prototypes des fonctions
+ */
+size_t print_list(const list_t *h);
+
+/* ... et d'autres prototypes pour les tâches suivantes ... */
+
+#endif /* LISTS_H */
